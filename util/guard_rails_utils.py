301a5f0dd63c3e778d3b9f9eb7e53e784f3be827
DESKTOP-E6C0MCN\adilr <adil.raza@fissionlabs.com>
2025-01-07 01:34:58 +0530

    list of bedrock guardrails

---
+++ util/guard_rails_utils.py
@@ -0,0 +1,31 @@
+import boto3
+from typing import Dict
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+class GuardRailsUtils:
+    @staticmethod
+    def get_bedrock_guardrails() -> Dict:
+        "Static method to fetch AWS Bedrock guardrails."
+
+        try:
+            client = boto3.client('bedrock')
+
+            logger.info("Fetching guardrails.")            
+            response = client.list_guardrails()
+            guardrails = response.get("guardrails", [])
+            logger.info("Guardrails fetched.")
+            
+            return [
+                    {
+                        "id": guardrail.get("id"), 
+                        "description": guardrail.get("description"), 
+                        "name": guardrail.get("name")
+                    }
+                    for guardrail in guardrails
+                ]
+        except Exception as e:
+            logger.error(f"Failed to fetch guardrails: {str(e)}")
+            raise Exception(f"Failed to fetch guardrails: {str(e)}")
