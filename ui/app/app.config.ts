a613b103da47bf43f36efe1880f083021543ac73
abdulgafoor <shaikabdul.gafoor@fissionlabs.com>
2025-02-12 10:17:49 +0530

    dropdown changes, header changes done

--- ui/app/app.config.ts
+++ ui/app/app.config.ts
@@ -20,6 +20,13 @@ export default defineAppConfig({
         // @ts-expect-error type inference
         size: "xl",
       },
+      slots : {
+        group: 'p-1 isolate-custom',
+        item : [
+          'w-full h-6 flex border my- border-gray-300 rounded'
+        ],
+        input: 'h-6',
+      }
     },
     inputNumber: {
       slots: {
